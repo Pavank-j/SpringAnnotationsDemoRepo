587130fe67ccae118e9296f360879785d36dc369
pavank-j <pavankjavadev6@gmail.com>
2020-06-14 11:10:57 -0500

    TennisCoach.java-afterTheBeanScopeDefined

--- SpringAnnotationsDemo/src/com/coders/springannotationsdemo/TennisCoach.java
+++ SpringAnnotationsDemo/src/com/coders/springannotationsdemo/TennisCoach.java
@@ -2,10 +2,12 @@ package com.coders.springannotationsdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 //@Component("thatSillyCoach")//beanID
 @Component//using default beanID"tennisCoach
+@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
